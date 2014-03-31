4c0dd640ed37e38242c419e79c9771beb8977692
Wu Peng <bill.1.wu@nokia.com>
2014-03-31 14:56:49 +0800

    init, not from the very begining.

---
+++ basetype.h
@@ -0,0 +1,16 @@
+#ifndef __BASE_TYPE__
+#define __BASE_TYPE__
+
+typedef signed char         S8;                                      
+typedef signed short        S16;                                     
+typedef unsigned char       U8;                                      
+typedef unsigned short      U16;                                     
+typedef signed int          S32;                                     
+typedef unsigned int        U32;      
+
+
+typedef int                 BOOL;               /* TRUE or FALSE */  
+typedef char                CHAR;               /* for char types */ 
+
+#endif
+
